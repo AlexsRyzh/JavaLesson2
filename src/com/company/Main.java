f5c1eee68e6d40fd6d91d7d030e74f712ec8f1b3
Alexs <alexsa>
2022-09-25 19:39:43 +0300

    complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Complex complex;
+        ConcreteFactory concreteFactory = new ConcreteFactory();
+
+        complex = concreteFactory.CreateComplex(1, 2);
+        System.out.println(complex.toString());
+
+        complex = concreteFactory.createComplex();
+        System.out.println(complex.toString());
     }
 }
