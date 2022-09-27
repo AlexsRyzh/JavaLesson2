91e422be744eafc77a6697f17c32b8622c22ef9d
Alexs <alexsa>
2022-09-27 09:31:10 +0300

    complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
+    public static void Filelist(String path){
+        File f = new File(path);
+        String[] fArray = null;
+        if (f.exists()||f.isDirectory()) fArray = f.list(null);
+        else System.out.print("Папка не найдена");
+        ArrayList<String> list = new ArrayList(Arrays.asList(fArray));
+
+        for (int i =0; i<list.size();i++) {
+            if(i<5) System.out.println(list.get(i));
+            else break;
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	    Filelist("F:\\Website");
     }
 }
