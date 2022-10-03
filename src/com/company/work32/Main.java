12bb1037428b08c73ff6c92f27d6d1da4ebf0f22
AlexsRyzh <alexsandr.ryzhkov04@gmai.com>
2022-10-03 22:12:37 +0300

    complete

--- src/com/company/work32/Main.java
+++ src/com/company/work32/Main.java
@@ -1,8 +1,7 @@
-package com.company;
+package com.company.work32;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        new OrdersManagerUI();
     }
 }
